b8b7603a4fb28dcf0b6910bf666bd3d53c991623
Ivan Gurov <guuuroov@gmail.com>
2023-03-07 11:11:49 +0300

    1.2

---
+++ lesson_1/1_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// 2. Напишите программу, которая будет выдавать название дня
+//    недели по заданному номеру.
+Console.WriteLine("Write a day number(1-7): ");
+int daynum = int.Parse(Console.ReadLine()!);
+
+
+if (daynum == 1)
+{
+  Console.WriteLine("Пн");
+}
+else if (daynum == 2)
+{
+  Console.WriteLine("Вт");
+}
+else if (daynum == 3)
+{
+  Console.WriteLine("Ср");
+}
+else if (daynum == 4)
+{
+  Console.WriteLine("Чт");
+}
+else if (daynum == 5)
+{
+  Console.WriteLine("Пт");
+}
+else if (daynum == 6)
+{
+  Console.WriteLine("Сб");
+}
+else if (daynum == 7)
+{
+  Console.WriteLine("Вс");
+}
+else
+{
+  Console.WriteLine("неизвестное число");
+}
