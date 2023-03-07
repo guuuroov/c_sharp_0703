3a8d120b4407f30dddb10aca3e54ab6122294892
Ivan Gurov <guuuroov@gmail.com>
2023-03-07 12:40:22 +0300

    homework4

---
+++ lesson_1/homework4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int result = 1;
+
+if(result==1)
+{
+  result+= 1;
+}
+while (result<=num)
+{
+  Console.Write(result);
+  result += 2;
+}
