1ba68f483e9a1509ab0280567abe5c7e3dd9a48a
Ivan Gurov <guuuroov@gmail.com>
2023-03-12 19:17:52 +0300

    homework2_2

---
+++ lesson_2/homework2_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int thDigit = (num /100)%10;
+
+
+
+if (thDigit ==0)
+{
+  Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+  Console.WriteLine("Третья цифра: " + thDigit);
+}
