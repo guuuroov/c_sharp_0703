982a3898312b50c795d6a9059db990c752cec695
Ivan Gurov <guuuroov@gmail.com>
2023-03-12 12:06:27 +0300

    lesson2

---
+++ lesson_2/2_0/Program.cs
@@ -0,0 +1,14 @@
+﻿//программа, принимающая трёхзначные числа 
+//и выводящая последнюю цифру этого числа
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return  num % 10; 
+    //10 - забирает последнюю цифру(пример 123 %10 -> 3)
+    //123/10 -> 12
+}
+
+int th_num = new Random().Next(100, 1000);
+int result=TakeNum(th_num);
+Console.WriteLine(result);
