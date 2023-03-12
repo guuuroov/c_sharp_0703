52ff1482a1fe9fb0f7a14d7dd0d8b629f2a44e27
Ivan Gurov <guuuroov@gmail.com>
2023-03-12 12:38:56 +0300

    2.1

---
+++ lesson_2/2_1/Program.cs
@@ -0,0 +1,36 @@
+﻿//программа, выводящая случайное число из отрезка(10,99)
+// и показывающая наибольшую цифру числа
+int num = GenNum();
+PrintNum(num);
+int dig1 = GetFirstDigit(num);
+int dig2 = GetSecDigit(num);
+int max = Max(dig1, dig2);
+PrintNum (max);
+
+
+int GenNum()
+{
+    int number = new Random().Next(10,100);
+    return number;
+}
+int GetSecDigit(int n)
+{
+    return n/10;
+}
+int GetFirstDigit(int n)
+{
+    return n%10;
+}
+int Max(int n1, int n2)
+{
+    if(n1>n2)
+    {
+      return n1;
+    }
+    return n2;
+}
+
+void PrintNum(int n)
+{
+  Console.WriteLine("Ваше число: " + n);
+}
