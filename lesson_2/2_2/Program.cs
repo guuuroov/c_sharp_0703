15cfc44c4e42d324bcceb30d6ae47ca977a03671
Ivan Gurov <guuuroov@gmail.com>
2023-03-12 13:24:30 +0300

    2.2

---
+++ lesson_2/2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// программа, которая принимает 2 числа
+// и выводит, является ли второе число кратным первому
+
+int num1 = int.Parse(Console.ReadLine()!); //запрос ввода числа
+int num2 = int.Parse(Console.ReadLine()!); //запрос ввода числа
+
+if(num1%num2 == 0) //кратность всегда %, 0 - деление без остатка
+{
+  Console.WriteLine("Yes");
+}
+else
+{
+  Console.WriteLine("No, "+ num1%num2);
+}
