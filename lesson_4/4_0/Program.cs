1c8c5f95ec87e0e164d16883c66bea3465b6aef7
Ivan Gurov <guuuroov@gmail.com>
2023-03-17 18:05:56 +0300

    4_0

--- lesson_4/4_0/Program.cs
+++ lesson_4/4_0/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Программа принимающая число (а) и выдающая сумму чисел от 1 до (а)
+
+Console.Write("Введите число: ");
+int SumNum(int num)
+{
+  int sum_all = 0;
+
+  for(int i = 1; i <= num; i++)
+      sum_all +=i;
+
+  return sum_all;
+}
+
+int val = int.Parse(Console.ReadLine()!);
+int result = SumNum(val);
+Console.WriteLine("Сумма чисел от 1 до введенного числа: " + result);
