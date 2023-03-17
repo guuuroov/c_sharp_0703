8eb893821f0e92c4163585a7157f8ba15148a7e4
Ivan Gurov <guuuroov@gmail.com>
2023-03-17 16:33:15 +0300

    3_3

---
+++ lesson_3/3_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа выдающая таблицу квадратов заданного числа
+
+Console.Write("Введите число: ");
+void Pow(int num)
+{
+    int i=1;
+    while(i<=num)
+    {
+        Console.WriteLine(Math.Pow(i,2));
+        i++;
+    }
+}
+
+int number=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Таблица квадратов: ");
+Pow(number);
