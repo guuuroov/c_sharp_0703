d71568da7ca1cb605db64a22bf6b8e9ae47590b2
Ivan Gurov <guuuroov@gmail.com>
2023-03-17 15:20:47 +0300

    3_1

--- lesson_3/3_1/Program.cs
+++ lesson_3/3_1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Программа, принимающая номер четверти системы координат
+// и выдающая диапазон возможных координат
+
+GetQuarter();
+
+void GetQuarter()
+{
+  Console.Write("Enter quarter: ");
+  int q = int.Parse(Console.ReadLine()!);
+  if(q < 1 || q > 4)
+  {
+    Console.WriteLine("Error! Quarter not found");
+  }
+  else
+  {
+    switch(q)
+    {
+      case 1:
+        Console.WriteLine("x>0;y>0");
+        break;
+      case 2:
+        Console.WriteLine("x<0;y>0");
+        break;
+      case 3:
+        Console.WriteLine("x<0;y<0");
+        break;
+      case 4:
+        Console.WriteLine("x>0;y<0");
+        break;
+      
+    }
+  }
+}
+
